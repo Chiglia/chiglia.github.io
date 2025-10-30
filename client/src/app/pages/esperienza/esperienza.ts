ec2916f886fb8b7b02c7c093ac209323c403c02c
Davide Chigliaro <davide.chigliaro@grupposcai.it>
2025-10-30 18:11:04 +0100

    Feat: pagina esperienza e pagina progetti migliorate

--- client/src/app/pages/esperienza/esperienza.ts
+++ client/src/app/pages/esperienza/esperienza.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared.module';
+import { AvatarModule } from 'primeng/avatar';
+import { FieldsetModule } from 'primeng/fieldset';
+import { StepperModule } from 'primeng/stepper';
 
 @Component({
   selector: 'app-esperienza',
-  imports: [],
+  imports: [SharedModule, AvatarModule, FieldsetModule, StepperModule],
   templateUrl: './esperienza.html',
   styles: ``,
 })
-export class Esperienza {
-
-}
+export class Esperienza {}
--- client/src/app/pages/progetti/progetti.ts
+++ client/src/app/pages/progetti/progetti.ts
@@ -10,24 +10,40 @@ import { SharedModule } from '../../shared.module';
 export class Progetti {
   projects = [
     {
-      name: 'Quiz di Programmazione',
+      id: 'findMySelf',
+      name: 'FindMySelf',
+      description: 'Insieme di quiz per aiutare le persone a conoscersi meglio',
+      link: 'https://quiz.chiglia.ovh',
+    },
+    {
+      id: 'makeCanzoniere',
+      name: 'Make Canzoniere',
+      description: 'Portale per creare e condividere canzonieri digitali',
       link: 'Quiz di Programmazione',
     },
     {
-      name: 'Quiz di Programmazione',
+      id: 'scout',
+      name: 'Scout',
+      description: 'Sito web per un gruppo scout locale',
       link: 'Quiz di Programmazione',
     },
     {
-      name: 'Quiz di Programmazione',
+      id: 'bluekippe',
+      name: 'Bluekippe',
+      description: 'Sito per società sportiva per disabili',
       link: 'Quiz di Programmazione',
     },
     {
-      name: 'Quiz di Programmazione',
+      id: 'parrocchia',
+      name: 'Parrocchia',
+      description: 'Sito web per una parrocchia locale',
       link: 'Quiz di Programmazione',
     },
     {
-      name: 'Quiz di Programmazione',
-      link: 'Quiz di Programmazione',
+      id: '1Hub',
+      name: '1Hub',
+      description: 'Piattaforma di aggregazione di risorse AI',
+      link: 'https://www.1hub.ai/app/#/',
     },
   ];
 }
