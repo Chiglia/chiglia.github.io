8a4d2f7f52ecbdfb132fa0eb7b75b18c9d3125a3
Davide Chigliaro <davide.chigliaro@grupposcai.it>
2025-10-30 18:41:38 +0100

    Feat: pagina competenze

--- client/src/app/pages/skills/skills.ts
+++ client/src/app/pages/skills/skills.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared.module';
+import { ProgressBarModule } from 'primeng/progressbar';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [SharedModule, ProgressBarModule],
   templateUrl: './skills.html',
   styles: ``,
 })
-export class Skills {
-
-}
+export class Skills {}
